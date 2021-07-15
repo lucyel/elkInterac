2bab07dc732dc3af81a7591edb56940442f05a50
Lucy <32479481+lucyel@users.noreply.github.com>
2021-07-15 09:44:54 +0700

    Escape $ char when run script in linux

--- setup.sh
+++ setup.sh
@@ -6,14 +6,14 @@ chmod 777 /var/log/users_history
 chmod +t /var/log/users_history
 
 cat << _EOF_ > /etc/profile.d/users_history.sh
-_fick=$(who am i|awk '{print $1}')
-_dis=$(id -u $_fick)
+_fick=\$(who am i|awk '{print $1}')
+_dis=\$(id -u $_fick)
 #if ["$_dis" > 0]
-if (( $_dis > 0 ))
+if (( \$_dis > 0 ))
 then
 	export HISTSIZE=10000
 	export HISTTIMEFORMAT='%F %T'
-	export HISTFILE=/var/log/users_history/history-users-$(who am I | awk '{print $1}';exit)-$(date +%F)
+	export HISTFILE=/var/log/users_history/history-users-\$(who am I | awk '{print $1}';exit)-\$(date +%F)
 	export PROMPT_COMMAND='history -a'
 fi
 _EOF_
@@ -21,6 +21,8 @@ _EOF_
 cat << __EOF__ >> /root/.bashrc
 export HISTSIZE=10000
 export HISTTIMEFORMAT='%F %T'
-export HISTFILE=/var/log/sudo_history/history-sudo-$(who am I | awk '{print $1}';exit)-$(date +%F)
+export HISTFILE=/var/log/sudo_history/history-sudo-\$(who am I | awk '{print $1}';exit)-\$(date +%F)
 export PROMPT_COMMAND='history -a'
 __EOF__
+
+source /root/.bashrc
